e6b6f17dfebe3919c4383aba7493834421f3a425
Edward T. Tanko <Edward T. Tanko@172.19.142.221>
2018-01-12 17:26:02 -0600

    Fourth Commit! [Optimizing search time, Optimizing Code]

--- index.php
+++ index.php
@@ -5,10 +5,9 @@ include_once './search_result.php';
 include_once './page_content_search.php';
 
 //List of pivot links
-$list_of_search_pivots = [
-    // "https://tankoedward.wordpress.com/", // Me
-    "https://www.killerphp.com/articles/", // Php
-        // "http://adambien.blog/roller/abien/",  // Java
+$list_of_search_pivots = [ 
+                "https://www.killerphp.com/articles/",  // a random php blog I found online 
+                "http://adambien.blog/roller/abien/"    // a random Java blog i found online 
 ];
 //check is search key word is present in request
 if (isset($_GET['search'])) {
@@ -22,15 +21,6 @@ if (isset($_GET['search'])) {
         $search_engine->setNum_of_sub_pivots(1);
         $search_engine->searchLink($search_key, $pivots);
     }
-    // Search using each word on the $search_key
-//         $words = explode($search_key, " ");
-//         if(count($words)>1){
-//            foreach($words as $key){
-//               foreach ($list_of_search_pivots as $pivots){
-//                 $search_engine->searchLink($key, $pivots);
-//               }
-//            }
-//         }
 
     $search_results = $search_engine->getSearch_result();
 } else {
@@ -41,29 +31,58 @@ if (isset($_GET['search'])) {
 <html>
     <head>
         <title>My Search Engine</title>
+        <style>
+            .mysearch_page{
+                text-align: left;
+                padding-top: 1%;
+                margin-left: 8%;
+                margin-right: 8%
+            }
+            .mysearch_page2{
+                text-align: center;
+                padding-top: 5%
+            }
+            .mysearch_page3{
+               font-style: italic
+            }
+            .mysearch_page4{
+              font-weight: bolder;
+              font-size: 80px;
+              color: skyblue
+            }
+            .mysearch_page5{
+              color: red
+            }
+            .mysearch_page6{
+                height: 50px;
+                width: 800px;
+                font-size: 20px
+            }
+        </style>
     </head>
     <body>
-        <div style="text-align: center;padding-top: 5%">
-            <div style="font-weight: bolder;font-size: 80px;color: skyblue">My Search Engine</div>
+        <div class="mysearch_page2">
+            <div class="mysearch_page4">My Search Engine</div>
             <form method="GET" action="./index.php">
-                <input name="search"  value="<?php echo (isset($_GET['search'])) ? $_GET['search'] : "" ?>"type="text" placeholder="Search ..."  style=" height: 50px;width: 800px;font-size: 20px"/>
+                <input name="search"  class="mysearch_page6"value="<?php echo (isset($_GET['search'])) ? $_GET['search'] : "" ?>"type="text" placeholder="Search ..." />
             </form>
 
             <?php if (isset($_GET['search'])) { ?>
-                <div style="text-align: left;padding-top: 1%;margin-left: 8%;margin-right: 8%">
+            <div class="mysearch_page">
                     <?php if (count($search_results) > 0) { ?>
                         <?php foreach ($search_results as $result) { ?>
                             <a href="<?php echo $result->getLink() ?>"><h2><u><?php echo $result->getTitle() ?></u></h2></a> 
                             <p><?php echo $result->getContent() ?></p> 
-                            <small style="font-style: italic">Search phase appears <b>[<?php echo $result->getSearch_key_count() ?>]</b> times</small> 
+                            <small class="mysearch_page3">Search phase appears <b>[<?php echo $result->getSearch_key_count() ?>]</b> times</small> 
                             <a href="<?php echo $result->getLink() ?>"><?php echo substr($result->getLink(), 0, 50) . "..." ?></a>
                             <hr/>
                         <?php } ?>
                     <?php } else { ?>
-                        <div style="color: red">No Results found! Please change your search words and try again</div>
+                        <div >No Results found! Please change your search words and try again</div>
                     <?php } ?>
                 </div>
             <?php } ?>
         </div>
     </body>
 </html>
+
